9515a39725039e95ba92dff7d805645769fc920d
Sunny <mahmud.moind.1@gmail.com>
2023-05-07 21:58:01 +0600

    adds .babelrc

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
-      breed: "Havenese",
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
       breed: "Cockatiel",
     }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
   ]);
 };
+
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
